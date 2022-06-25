cde0fe017a1ba038fd0994176815116e1e9b2930
Supakit <supakit.pnm@gmail.com>
2022-06-25 13:27:36 +0700

    Add: Router Programatically

--- src/pages/ProductDetails.vue
+++ src/pages/ProductDetails.vue
@@ -9,14 +9,18 @@
 
 <script>
 import { inject, computed } from 'vue';
+import { useRoute } from 'vue-router';
 
 export default {
   props: ['pid'],
-  setup(props) {
+  setup() {
+    const route = useRoute();
+
     const products = inject('products');
-    const selectedProduct = computed(() => products.value.find(
-      product => product.id === props.pid
-    ));
+    const selectedProduct = computed(() =>
+      products.value.find((product) => product.id === route.params.pid)
+    );
+
     const title = computed(() => selectedProduct.value.title);
     const price = computed(() => selectedProduct.value.price);
     const description = computed(() => selectedProduct.value.description);
